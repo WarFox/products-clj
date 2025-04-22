1ac3c45c7577902324d0fd277ff1bfdf4aee9317
Deepu Mohan Puthrote <186013+WarFox@users.noreply.github.com>
2025-04-22 19:23:11 +0100

    test: refactor test fixtures with-db and given-table

--- test/app/products_test.clj
+++ test/app/products_test.clj
@@ -8,15 +8,25 @@
 
 (def ^:dynamic *db* (atom nil))
 
+(defn with-db
+  "A fixture that sets up a PostgreSQL container for testing."
+  [f]
+  (let [container (containers/postgres-container system/db-spec)
+        port      (-> container :mapped-ports (get 5432))
+        db-spec   (assoc system/db-spec :port port)]
+    (reset! *db* db-spec)
+    (f)
+    (containers/stop! container)))
+
+(defn given-table
+  "A fixture that sets up a PostgreSQL container and creates a table for testing."
+  [f]
+  (db/create-table @*db*)
+  (f))
+
 (t/use-fixtures :once
-  (fn [f]
-    (let [container (containers/postgres-container system/db-spec)
-          port      (-> container :mapped-ports (get 5432))
-          db-spec   (assoc system/db-spec :port port)]
-      (reset! *db* db-spec)
-      (db/create-table db-spec)
-      (f)
-      (containers/stop! container))))
+  with-db
+  given-table)
 
 (t/deftest create-product-test
   (t/testing "Create a product"
@@ -40,8 +50,8 @@
                    :price-in-cents 100
                    :created-at     (Instant/now)
                    :updated-at     (Instant/now)}]
-      (products/create-product {:db @*db*
-                           :body-params product}) ; Create a product for testing
+      (products/create-product {:db          @*db*
+                                :body-params product}) ; Create a product for testing
       (t/is (= {:status 200
                 :body   [product]}
                (products/get-products {:db @*db*}))))))
